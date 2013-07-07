0b3507d8c22d43c8cf707a91430d754cf871cef1
Darren Liew <ldarren@gmail.com>
2013-07-07 15:26:24 +0000

    changed ui layout for rogue sample

--- samples/rogue/js/uiPlayer.js
+++ samples/rogue/js/uiPlayer.js
@@ -10,11 +10,12 @@ pico.def('uiPlayer', 'picBase', function(){
     me.draw = function(ctx, ent, clip){
         var o = ent.getComponent(name);
         if (!o) return;
-
+/*
         ctx.save();
         ctx.beginPath();
         ctx.fillStyle = 'rgba(172, 152, 102, 1)';
         ctx.fillRect(clip[0], clip[1], clip[2], clip[3]);
         ctx.restore();
+*/
     };
 });
