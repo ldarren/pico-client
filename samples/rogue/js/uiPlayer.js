8d374facadf20c7873a84228926b25c8e1c89076
Darren Liew <ldarren@gmail.com>
2013-07-15 15:07:28 +0000

    improved clear background method

--- samples/rogue/js/uiPlayer.js
+++ samples/rogue/js/uiPlayer.js
@@ -3,7 +3,27 @@ pico.def('uiPlayer', 'picBase', function(){
     var
     me = this,
     name = me.moduleName,
-    layouts = [];
+    layouts = [],
+    fitGridMap = function(rect, gzX, gzY, smaller){
+        var
+        roundTL = smaller ? Math.ceil : Math.floor,
+        roundBR = smaller ? Math.floor : Math.ceil,
+        t = rect[1],
+        l = rect[0],
+        r = l + rect[2],
+        b = t + rect[3];
+
+        t = roundTL(t / gzY)*gzY;
+        l = roundTL(l / gzX)*gzX;
+        b = roundBR(b / gzY)*gzY;
+        r = roundBR(r / gzX)*gzX;
+
+        rect[0] = l;
+        rect[1] = t;
+        rect[2] = r - l;
+        rect[3] = b - t;
+        return rect;
+    };
 
     me.create = function(ent, data){
         var
@@ -12,15 +32,16 @@ pico.def('uiPlayer', 'picBase', function(){
         b = theme.BORDERS,
         borders = this.borders;
 
-        borders.push(ts.cut(b.TOP));
-        borders.push(ts.cut(b.LEFT));
-        borders.push(ts.cut(b.BOTTOM));
-        borders.push(ts.cut(b.RIGHT));
-
         data.active = false;
         data.maximized = 0;
+        var gs = data.gridSize = this.smallDevice ? 8 : 16;
         data.minWidth = this.smallDevice ? 320 : 640;
         data.minHeight = this.tileHeight;
+
+        borders.push(ts.cut(b.TOP, gs, gs));
+        borders.push(ts.cut(b.LEFT, gs, gs));
+        borders.push(ts.cut(b.BOTTOM, gs, gs));
+        borders.push(ts.cut(b.RIGHT, gs, gs));
 
         return data;
     };
@@ -35,14 +56,17 @@ pico.def('uiPlayer', 'picBase', function(){
         if (!com) return entities;
 
         var
+        gs = com.gridSize,
         boxName = com.box,
         boxCom = e.getComponent(boxName),
         layout;
 
         layouts.length = 0;
 
-        layouts.push([evt[0] + Math.floor((evt[2] - com.minWidth)/2), evt[1] + evt[3] - com.minHeight, com.minWidth, com.minHeight]);
-        layouts.push([evt[0], evt[1], evt[2], evt[3]]);
+        layouts.push(fitGridMap(
+            [evt[0] + Math.floor((evt[2] - com.minWidth)/2), evt[1] + evt[3] - com.minHeight, com.minWidth, com.minHeight],
+            gs, gs, false));
+        layouts.push(fitGridMap([evt[0]+1, evt[1]+1, evt[2]-2, evt[3]-2], gs, gs, true));
 
         layout = layouts[com.maximized];
 
@@ -62,6 +86,9 @@ pico.def('uiPlayer', 'picBase', function(){
             if (!uiOpt) continue;
             rectOpt = e.getComponent(uiOpt.box);
             active = (rectOpt.x < evt.x && (rectOpt.x + rectOpt.width) > evt.x && rectOpt.y < evt.y && (rectOpt.y + rectOpt.height) > evt.y);
+            if (active !== uiOpt.active){
+                uiOpt.active = active;
+            }
             if (active){
                 uiOpt.maximized = uiOpt.maximized ? 0 : 1;
                 var layout = layouts[uiOpt.maximized];
@@ -69,11 +96,9 @@ pico.def('uiPlayer', 'picBase', function(){
                 rectOpt.y = layout[1];
                 rectOpt.width = layout[2];
                 rectOpt.height = layout[3];
-            }
-            if (active !== uiOpt.active){
-                uiOpt.active = active;
                 return [e];
             }
+            break;
         }
         return entities;
     };
@@ -84,6 +109,7 @@ pico.def('uiPlayer', 'picBase', function(){
         if (!uiOpt) return;
 
         var
+        gs = uiOpt.gridSize,
         rectOpt = ent.getComponent(uiOpt.box),
         ts = this.tileSet,
         theme, borders = this.borders;
@@ -100,21 +126,25 @@ pico.def('uiPlayer', 'picBase', function(){
         ctx.fillRect(rectOpt.x, rectOpt.y, rectOpt.width, rectOpt.height);
 
         ctx.fillStyle = ctx.createPattern(borders[0], 'repeat');
-        ctx.fillRect(rectOpt.x, rectOpt.y, rectOpt.width, 8);
+        ctx.fillRect(rectOpt.x, rectOpt.y, rectOpt.width, gs);
 
         ctx.fillStyle = ctx.createPattern(borders[3], 'repeat');
-        ctx.fillRect(rectOpt.x + rectOpt.width - 8, rectOpt.y, 8, rectOpt.height);
-
-        ctx.fillStyle = ctx.createPattern(borders[2], 'repeat');
-        ctx.fillRect(rectOpt.x, rectOpt.y+rectOpt.height-8, rectOpt.width, 8);
+        ctx.fillRect(rectOpt.x + rectOpt.width - gs, rectOpt.y, gs, rectOpt.height);
 
         ctx.fillStyle = ctx.createPattern(borders[1], 'repeat');
-        ctx.fillRect(rectOpt.x, rectOpt.y, 8, rectOpt.height);
+        ctx.fillRect(rectOpt.x, rectOpt.y, gs, rectOpt.height);
 
-        ts.draw(ctx, theme.TOP_LEFT, rectOpt.x, rectOpt.y);
-        ts.draw(ctx, theme.TOP_RIGHT, rectOpt.x + rectOpt.width - 8, rectOpt.y);
-        ts.draw(ctx, theme.BOTTOM_LEFT, rectOpt.x, rectOpt.y + rectOpt.height - 8);
-        ts.draw(ctx, theme.BOTTOM_RIGHT, rectOpt.x + rectOpt.width - 8, rectOpt.y + rectOpt.height - 8);
+        ts.draw(ctx, theme.TOP_LEFT, rectOpt.x, rectOpt.y, gs, gs);
+        ts.draw(ctx, theme.TOP_RIGHT, rectOpt.x + rectOpt.width - gs, rectOpt.y, gs, gs);
+
+        if (uiOpt.maximized){
+            ctx.fillStyle = ctx.createPattern(borders[2], 'repeat');
+            ctx.fillRect(rectOpt.x, rectOpt.y+rectOpt.height-gs, rectOpt.width, gs);
+
+            ts.draw(ctx, theme.BOTTOM_LEFT, rectOpt.x, rectOpt.y + rectOpt.height - gs, gs, gs);
+            ts.draw(ctx, theme.BOTTOM_RIGHT, rectOpt.x + rectOpt.width - gs, rectOpt.y + rectOpt.height - gs, gs, gs);
+        }
+
         ctx.restore();
 
     };
