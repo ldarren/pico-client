424b5e994c101149f7c1b09874845f5d05e360bc
Darren Liew <ldarren@gmail.com>
2013-07-24 13:51:40 +0000

    fixed go o next tile func

--- samples/rogue/js/game.js
+++ samples/rogue/js/game.js
@@ -209,26 +209,29 @@ pico.def('game', 'picGroup', function(){
         mapW = this.mapWidth,
         mapH = this.mapHeight,
         h,
-        check = function(){return (undefined !== h && !(h & G_TILE_TYPE.HIDE));};
+        check = function(){
+            var m = map[h];
+            return (undefined !== m && !(m & G_TILE_TYPE.HIDE));
+        };
 
-        h = map[i+mapW];
+        h = i+mapW;
         if (check()) return h;
-        h = map[i-mapW];
+        h = i-mapW;
         if (check()) return h;
         if (0 !== (i%mapW)){
-            h = map[i-1];
+            h = i-1;
             if (check()) return h;
-            h = map[i-mapW-1];
+            h = i-mapW-1;
             if (check()) return h;
-            h = map[i+mapW-1];
+            h = i+mapW-1;
             if (check()) return h;
         }
         if (0 !== ((i+1)%mapW)){
-            h = map[i+1];
+            h = i+1;
             if (check()) return h;
-            h = map[i-mapW+1];
+            h = i-mapW+1;
             if (check()) return h;
-            h = map[i+mapW+1];
+            h = i+mapW+1;
             if (check()) return h;
         }
         return -1;
