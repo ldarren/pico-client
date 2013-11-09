94e1c12d957be081e8c16097b38661ca29c4baa2
Darren Liew <ldarren@gmail.com>
2013-11-09 09:26:26 +0000

    fixed audio problem, should play on android and ios

--- samples/match3/js/match3.js
+++ samples/match3/js/match3.js
@@ -1,12 +1,9 @@
 pico.def('m3Main', function(){
-    this.use('piHTMLAudio');
 
     var
     me = this,
     onLoad = function(){
-        me.piHTMLAudio.create('dat/ui-sfx.json', function(err, audioSprite){
-            pico.addFrame(document.body, 'div#page', 'html/login.html');
-        });
+        pico.addFrame(document.body, 'div#page', 'html/login.html');
     };
 
     me.slot(pico.LOAD, onLoad);
