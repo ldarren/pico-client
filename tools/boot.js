0f033bd6f61f29a8051efcd9b91258dda5d6e7dc
Darren Liew <ldarren@gmail.com>
2014-07-27 06:29:08 +0000

    added replace transform

--- tools/boot.js
+++ tools/boot.js
@@ -3,14 +3,42 @@
 var
 fs = require('fs'),
 path = require('path'),
-templDir = path.dirname(process.argv[1])+'/boot/',
-projectName = process.argv[2]
+Transform = require('stream').Transform,
+util = require('util'),
+srcDir = path.dirname(process.argv[1])+'/boot/',
+projDir = process.argv[2],
+projId = process.argv[3],
+projName = process.argv[4],
+projURL = process.argv[5],
+projLib = process.argv[6]
 
-fs.mkdir(projectName, 0777, function(){
+if (!projDir || !projId || !projName || !projSvr || !projLib)
+    return console.log('USAGE: '+process.argv[1]+' dir_name projId projName lib_path')
+
+function ReplaceTransform(options){
+    if (!(this instanceof ReplaceTransform)) return new ReplaceTransform(options)
+
+    this.replace = {
+        PROJ_NAME: projName,
+        PROJ_ID: projId,
+        PROJ_URL: projURL
+    }
+
+    Transform.call(this, options)
+}
+util.inherits(ReplaceTransform, Transform)
+
+ReplaceTransform.prototype._transform = function(chunk, encoding, cb) {
+    console.log('chunk: '+chunk.length)
+    this.push(chunk, encoding)
+    cb(null)
+}
+
+fs.mkdir(projDir, 0777, function(){
     (function(dirs, cb){
         if (!dirs.length) return cb()
         var next = arguments.callee
-        fs.mkdir(projectName+'/'+dirs.shift(), 0777, function(){
+        fs.mkdir(projDir+'/'+dirs.shift(), 0777, function(){
             next(dirs, cb) 
         })
     })(['js', 'html', 'css', 'dat','js/modules'], function(){
@@ -18,8 +46,10 @@ fs.mkdir(projectName, 0777, function(){
             if (!files.length) return cb()
             var
             fname = files.shift()
-            dest = projectName + path.sep + files.shift() + path.sep + fname
-            fs.createReadStream(templDir+fname).pipe(fs.createWriteStream(dest))
+            dest = projDir + path.sep + files.shift() + path.sep + fname
+            fs.createReadStream(srcDir+fname)
+            .pipe(new ReplaceTransform({decodeStrings:false}))
+            .pipe(fs.createWriteStream(dest))
             arguments.callee(files, cb)
         })([
             'index.html','',
@@ -33,9 +63,9 @@ fs.mkdir(projectName, 0777, function(){
             'Model.js','js',
             'frame.html','html'
             ], function(){
-            console.log('done')
+            fs.symlink(projLib, projDir+'/lib', function(){
+                console.log('done')
+            })
         })
-    })
-    fs.symlink('../lib/', projectName+'/lib', function(){
     })
 })
