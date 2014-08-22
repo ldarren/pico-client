38d81f47b008ad376260292a5d2b5e5364c95d59
Darren Liew <ldarren@gmail.com>
2014-08-22 04:17:02 +0000

    rename header to reinits

--- samples/studio/js/views/Project.js
+++ samples/studio/js/views/Project.js
@@ -196,7 +196,7 @@ exports.Class = Panel.Class.extend({
 
         if (!pageId) alert('please enter a name')
 
-        pages[pageId] = {header:{},spec:[],styles:[]}
+        pages[pageId] = {reinits:{},spec:[],styles:[]}
         pageList[pageId] = new Page({title:pageId,page:pages[pageId]})
         pageList[pageId].render(this.$el)
         this.showPage(pageId)
