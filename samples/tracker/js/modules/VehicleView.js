9a50d4f108ba5a059a7e5b577818b7127d8d3529
Darren Liew <ldarren@gmail.com>
2014-09-28 17:17:26 +0000

    added vehicle

---
+++ samples/tracker/js/modules/VehicleView.js
@@ -0,0 +1,43 @@
+var
+Module = require('Module'),
+Router = require('Router'),
+tpl = require('@html/View.html'),
+common = require('modules/common')
+
+exports.Class = Module.Class.extend({
+    create: function(spec){
+        var
+        vehicle = this.require('vehicle').value.attributes,
+        owner = this.require('owner').value,
+        data = this.require('data').value,
+        mi = data.get(owner.models[0].id),
+        isAdmin = common.isAdminAbove(mi.get('user')),
+        detail = vehicle.json,
+        fields = [], actions = []
+
+        if (isAdmin) this.triggerHost('changeHeader', {title:detail.tag, right:['edit']})
+
+        this.vehicle = vehicle
+        this.owner = owner
+        fields.push({label:'Plate #', value:detail.tag}) 
+        fields.push({label:'Seater', value:detail.seater}) 
+        fields.push({label:'Model', value:detail.model}) 
+        fields.push({label:'Join Date', value:(new Date(vehicle.createdAt)).toLocaleDateString(common.getLang(), common.getDateFormat())})
+
+        if (isAdmin) actions.push({icon:'btn-negative', name:'delete', text:'Remove'})
+
+        this.$el.html(_.template(tpl.text, {fields:fields, actions:actions}))
+    },
+    moduleEvents: function(evt, sender){
+        switch(evt){
+        case 'edit': Router.instance().nav('vehicle/edit/'+this.vehicle.id); break
+        default: Module.Class.prototype.moduleEvents.apply(this, arguments)
+        }
+    },
+    events: {
+        'click button[name=delete]': 'del'
+    },
+    del: function(e){
+        this.vehicle.destroy()
+    }
+})
