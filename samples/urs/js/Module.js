a28eb6a7847730867a4734c60d9558cb7c1629d9
Darren Liew <ldarren@gmail.com>
2015-07-20 00:35:16 +0800

    added urs, added cfg folder, rm old libs

---
+++ samples/urs/js/Module.js
@@ -0,0 +1,208 @@
+var
+ID=0,TYPE=1,VALUE=2,EXTRA=3,
+specMgr = require('specMgr'),
+Router = require('Router'),
+index=0,
+trigger = Backbone.Events.trigger,
+evts=[],
+schedule= (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      || 
+            window.msRequestAnimationFrame     ||
+            function(callback){ return window.setTimeout(callback, 50) }
+})(),   
+specLoaded = function(err, spec, self){
+    if (self._removed) return self.remove()
+    if (err){
+        console.warn(err)
+        return Router.instance.home()
+    }
+
+    self.spec = spec
+
+    var
+    d = {},
+    deps = self.deps || {}
+
+    for(var i=0,s,k; s=spec[i]; i++){
+        k = s[ID]
+        if (deps[k]) d[k] = s[VALUE]
+    }
+
+    self.deps = d
+    self.create(d)
+},
+sigslot = function(){
+    var
+    ss = this.signals || [],
+    signals = {}
+    
+    ss.forEach(function(evt){
+        var sender = this
+        signals[evt] = function(){
+            return {
+                args: Array.prototype.slice.call(arguments),
+                sender: sender,
+                evt: evt,
+                queue: false,
+                send: send,
+                dispatch: dispatch
+            }
+        }
+    }, this)
+        
+    return signals
+},      
+send = function(a, from){
+    this.queue=true
+    evts.push([this, a, from||this.sender])
+},      
+recv = function(evt, from, params){
+    var 
+    func = this.slots[evt],
+    forward = true 
+                
+    if (func) forward = func.apply(this, [from, params.sender].concat(params.args))
+    if (forward) (params.queue?send:dispatch).call(params, [from], this)
+},
+tick = function(){
+    schedule(tick)
+    if (evts.length){
+        var e=evts.shift()
+        dispatch.call(e[0], e[1], e[2])
+    }
+},
+dispatch = function(a, from){
+    var isObj='object'===typeof a
+    if (isObj && !a.length) return trigger.call(a, this.evt, from, this)
+
+    from=from||this.sender
+
+    var
+    host = from.host,
+    modules = from.modules
+
+    modules = host ? modules.concat([host]) : modules
+
+    if (isObj && a.length){
+        for(var i=0,m; m=modules[i]; i++) if (-1 === a.indexOf(m)) trigger.call(m, this.evt, from, this);
+    }else{
+        for(var i=0,m; m=modules[i]; i++) trigger.call(m, this.evt, from, this);
+    }
+}
+
+schedule(tick)
+
+exports.Class = Backbone.View.extend({
+    initialize: function(options, spec, params, host){
+        this._id = index++
+        this.name = options.name
+        this.host = host
+        this.ancestor = exports.Class.prototype
+        this.modules = []
+        this._elements = []
+        this._rawSpec = spec
+        this._removed = false 
+
+        this.signals = sigslot.call(this)
+
+        this.on('all', recv, this)
+
+        if (options.style) this.style = restyle(options.style, ['webkit'])
+
+        specMgr.load(host, params || [], spec, specLoaded, this)
+    },
+    create: function(deps, params){
+        var spec = this.spec
+        for(var i=0,s; s=spec[i]; i++){
+            if ('module' === s[TYPE]) {
+                this.spawn(s[VALUE], params)
+            }
+        }
+    },
+    remove: function(){
+        this._removed = true 
+        this.off()
+        if (this.__proto__.el){
+            // dun remove things not urs
+            this.$el.empty()
+            this.stopListening()
+            this.undelegateEvents()
+        }else{
+            Backbone.View.prototype.remove.apply(this, arguments)
+        }
+        this.dumpAll()
+        if (this.style) this.style.remove()
+        specMgr.unload(this._rawSpec, this.spec)
+    },
+    spawn: function(Mod, params, spec, hidden){
+        if (!Mod || !Mod.spec) return
+
+        var
+        m = new (exports.Class.extend(Mod.Class))(Mod, spec && spec.length ? Mod.spec.concat(spec) : Mod.spec, params, this),
+        i = this.modules.push(m)-1
+
+        if (hidden) return m
+
+        var el = m.render()
+        if (el) {
+            this.el.appendChild(el)
+            this._elements[i] = el
+        }
+        return m
+    },
+    dump: function(mod){
+        if (!mod) return
+        mod.remove()
+        this.hide(mod)
+        var i = this.modules.indexOf(mod)
+        this.modules.splice(i, 1)
+        this._elements.splice(i, 1)
+    },
+    dumpAll:function(){
+        for(var i=0,ms=this.modules,m; m=ms[i]; i++){
+            m.remove()
+            this.hide(m)
+        }
+        ms.length = 0
+        this._elements.length = 0
+    },
+    show: function(mod, host, first){
+        host = host || this.el
+
+        var
+        i = this.modules.indexOf(mod),
+        oldEl = this._elements[i],
+        el = mod.render()
+        if (el){
+            if (host.contains(oldEl)){
+                host.replaceChild(el, oldEl)
+            }else{
+                if (first) host.insertBefore(el, host.firstChild)
+                else host.appendChild(el)
+            }
+            this._elements[i] = el
+        }
+        return el
+    },
+    hide: function(mod, host){
+        host = host || this.el
+
+        var
+        i = this.modules.indexOf(mod),
+        oldEl = this._elements[i]
+        if (oldEl && host.contains(oldEl)){
+            host.removeChild(oldEl)
+        }
+        return oldEl
+    },
+    render: function(){
+        return this.el
+    },
+    slots:{
+        // seldom use, useful only after BB's setElement
+        invalidate: this.show
+    }
+})
---
+++ samples/urs/js/specMgr.js
@@ -0,0 +1,113 @@
+var
+Model = require('Model'),
+ID=0,TYPE=1,VALUE=2,EXTRA=3,
+ERR1='ref of REF not found',ERR2='record RECORD of ref of REF not found',
+create = function(id, type, value){ return [id, type, value] },
+getId = function(spec){return spec[ID]},
+getType = function(spec){return spec[TYPE]},
+getValue = function(spec){return spec[VALUE]},
+getExtra = function(spec,idx){return spec[EXTRA+(idx||0)]},
+find = function(name, list){ for(var i=0,o; o=list[i]; i++){ if (name === o[ID]) return o } },
+findAll = function(type, list){
+    var arr = []
+    for(var i=0,o; o=list[i]; i++){ if (type === o[TYPE]) arr.push(o) }
+    return arr
+},
+loadDeps = function(links, idx, klass, cb){
+    if (!links || links.length <= idx) return cb(null, klass)
+    if ('string' === typeof links) return require(links, function(err, mod){
+        return cb(err, mod.Class)
+    })
+    require(links[idx++], function(err, mod){
+        if (err) return cb(err)
+        loadDeps(links, idx, pico.obj.extend(klass, mod.Class), cb)
+    })
+},
+load = function(host, params, spec, deps, cb, userData){
+    if (!spec.length) return cb(null, deps, userData)
+
+    var
+    context = host ? host.spec : [],
+    s = spec.shift(),
+    t = s[TYPE],
+    f
+
+    switch(t){
+    case 'ref':
+        f = find(s[VALUE], context)
+		if (!f) return cb(ERR1.replace('REF', s[VALUE]), deps, userData)
+        deps.push(create(s[ID], f[TYPE], f[VALUE]))
+        break
+    case 'refs':
+        Array.prototype.push.apply(deps, findAll(s[VALUE], context))
+        break
+    case 'model':
+		f = find(s[VALUE], context)
+		if (!f) return cb(ERR1.replace('REF', s[VALUE]), deps, userData)
+		var m = f[VALUE].get(params[s[EXTRA]])
+		if (!m || !m.get) return cb(ERR2.replace('REF', s[VALUE]).replace('RECORD',params[s[EXTRA]]), deps, userData)
+		'field' === t ? deps.push(create(s[ID], t, m.get(s[EXTRA+1]))) : deps.push(create(s[ID], t, m)) 
+		break
+    case 'models':
+        deps.push(create(s[ID], t, new Model.Class(null, s[VALUE])))
+        break
+	case 'fields':
+		f = find(s[VALUE], context)
+		if (!f) return cb(ERR1.replace('REF', s[VALUE]), deps, userData)
+		var m = s[EXTRA] ? new Model.Class(f[VALUE].where(s[EXTRA])) : f[VALUE]
+		if (!m || !m.pluck) return cb(ERR2.replace('REF', s[VALUE]).replace('RECORD',s[EXTRA]), deps, userData)
+		deps.push(create(s[ID], t, m.pluck(s[EXTRA+1])))
+		break
+    case 'module':
+        loadDeps(s[EXTRA]||s[ID], 0, {}, function(err, klass){
+            if (err) return cb(err, deps, userData)
+            f=s[ID]
+            f='string'===typeof f ? f : f[0]
+            deps.push(create(f, t, {name:f, spec:s[VALUE], Class:klass}))
+            load(host, params, spec, deps, cb, userData)
+        })
+        return
+    case 'param':
+        deps.push(create(s[ID], t, params[s[VALUE]]))
+        break
+    case 'time':
+    case 'date':
+    case 'datetime':
+        deps.push(create(s[ID], t, new Date(s[VALUE])))
+        break
+    default:
+        deps.push(create(s[ID], t, s[VALUE]))
+        break
+    }
+    load(host, params, spec, deps, cb, userData)
+},
+// need to get original spec, the one before spec.load, no way to diff ref and models
+unload = function(rawSpec, spec){
+    if (!spec || !spec.length) return
+    for(var i=0,r; r=rawSpec[i]; i++){
+        switch(r[TYPE]){
+        case 'models':
+        case 'date':
+            for(var j=0,s; s=spec[i]; i++){
+                if (r[ID] === s[ID]) {
+                    if ('models' === s[TYPE]) s[VALUE].reset()
+                    delete s[VALUE]
+                }
+            }
+            break
+        }
+    }
+    spec.length = 0
+}
+
+exports.load = function(host, params, spec, cb, userData){
+    load(host, params, spec.slice(), [], cb, userData)
+}
+exports.unload = unload
+exports.find = find
+exports.findAll = findAll
+exports.create = create
+exports.getId = getId
+exports.getType = getType
+exports.getValue = getValue
+exports.getExtra = getExtra
