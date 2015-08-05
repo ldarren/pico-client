2ecb29fe63aea3a10c3e02e5539b034eecb75a15
Darren Liew <ldarren@gmail.com>
2015-08-05 07:20:14 +0000

    Stream auto reconnect

--- skeleton/Stream.js
+++ skeleton/Stream.js
@@ -2,21 +2,24 @@
 var network=require('network')
 
 exports.Class=function(options){
-    init(this, network.createEvents(options.url, options.withCredentials), options.events)
-}
-
-function init(self, sse, events){
-    self.sse=sse
-    sse.addEventListener('open', function(e){
+    init(this, options.url, options.withCredentials, options.events)
+}           
+            
+function init(self, url, withCredentials, events){
+    self.sse=network.createEvents(url, withCredentials)
+    self.sse.addEventListener('open', function(e){
         self.trigger(e.type)
-    }, false)
-    sse.addEventListener('error', function(e){
+    }, false)   
+    self.sse.addEventListener('error', function(e){
         switch(e.target.readyState){
-        case EventSource.CLOSED: self.trigger('closed'); break
         case EventSource.CONNECTING: self.trigger('connecting'); break
-        default: self.trigger('error'); vreak
-        }
-    }, false)
+        case EventSource.CLOSED:
+        default:
+            self.trigger('closed');
+            setTimeout(init, 5000, self, url, withCredentials, events)
+            break
+        }       
+    }, false)   
     var trigger=function(e){
         var data
         try{ data=JSON.parse(e.data) }
@@ -24,9 +27,9 @@ function init(self, sse, events){
         self.trigger(e.type, data, e.lastEventId)
     }
     for(var i=0,e; e=events[i]; i++){
-        sse.addEventListener(e,trigger,false)
-    }
-}
+        self.sse.addEventListener(e,trigger,false)
+    }       
+}       
 
 _.extend(exports.Class.prototype, Backbone.Events,{
     close:function(){
