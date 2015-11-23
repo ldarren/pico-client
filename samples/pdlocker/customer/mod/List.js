5c4994ef9c4b39b5ad1814446a0174fea3cc6f09
Darren Liew <ldarren@gmail.com>
2015-11-23 08:16:33 +0000

    fixed view after ctrl in _spawnList become hidden issue

--- samples/pdlocker/customer/mod/List.js
+++ samples/pdlocker/customer/mod/List.js
@@ -1,14 +1,24 @@
+var tpl='<button class="btn btn-positive btn-block">Unlock</button><button class="btn btn-negative btn-block">Lock</button>'
 return{
+    className:'content-padded',
 	signals:['ble_startScan'],
 	deps:{
         owner:'models'
 	},
 	create: function(deps){
-        if (deps.owner.length) this.signals.ble_startScan([],30).send(this.host)
+        if (!deps.owner.length) return
+        this.signals.ble_startScan([],30).send(this.host)
+        this.el.innerHTML=tpl
 	},
 	slots:{
 		ble_scan:function(from,sender,peripheral){
 			console.log(peripheral)
-		}
+            if ('AF131569'===peripheral.name){
+                this.signals.ble_connect(peripheral.id).send(sender)
+            }
+		},
+        ble_connected:function(from,sender,peripheral){
+            console.log
+        }
 	}
 }
