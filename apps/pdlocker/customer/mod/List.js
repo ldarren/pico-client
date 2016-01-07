a22c049bc7ba07043757e1705189ec4fe566613c
Darren Liew <ldarren@gmail.com>
2016-01-08 00:36:15 +0800

    removed lock

--- apps/pdlocker/customer/mod/List.js
+++ apps/pdlocker/customer/mod/List.js
@@ -5,11 +5,21 @@ scratch2='A495FF22-C5B1-4B44-B512-1370F02D74DE',
 scratch3='A495FF23-C5B1-4B44-B512-1370F02D74DE',
 scratch4='A495FF24-C5B1-4B44-B512-1370F02D74DE',
 scratch5='A495FF25-C5B1-4B44-B512-1370F02D74DE',
-tpl='<button class="btn btn-positive btn-block">Unlock</button><button class="btn btn-negative btn-block">Lock</button>'
+tpl='<button class="btn btn-positive btn-block">Unlock</button>',
+passcode=65535,
+salt=456,
+credential=function(){
+	var
+	key=Math.floor(Math.random()*0xffffffff),
+	hash=passcode^key
+
+	key+=salt
+	return [hash, key]
+}
 
 return{
     className:'content-padded',
-	signals:['ble_startScan','ble_connect','ble_write'],
+	signals:['ble_startScan','ble_connect','ble_write','ble_startNotification','ble_stopNotification'],
 	deps:{
         owner:'models'
 	},
@@ -36,25 +46,37 @@ return{
             console.log('disconnected',peripheral)
 			this.peripheral=null
 			this.ble=null
-        }
+        },
+		ble_notification:function(from,sender,err,buffer){
+			if (err) return console.error(err)
+			var stage=new Uint32Array(buffer)
+			var btn=this.el.querySelector('button')
+			switch(stage[0]){
+			case 1:
+				btn.setAttribute('disable',1)
+				btn.textContent='Unlocked'
+				break
+			case 2:
+				btn.removeAttribute('disable')
+				btn.textContent='Unlock'
+				this.signals.ble_stopNotification(this.peripheral.id, scratchSrv, sratch3)
+				break
+			}
+		}
 	},
 	events:{
 		'tap .btn-positive':function(e){
-			if (!this.peripheral) return
-			var data = new Uint8Array(1);
-			data[0] = 0;
-			this.signals.ble_write(this.peripheral.id, scratchSrv, scratch1, data.buffer, function(){
-				debugger
+			var
+			self=this,
+			p=this.peripheral
+			if (!p) return
+			var cred=Uint32Array(credential())
+			this.signals.ble_startNotification(p.id, scratchSrv, scratch3)
+			this.signals.ble_write(p.id, scratchSrv, scratch1, cred.buffer.slice(0, 4), function(){
 				console.log(arguments)
-			}).send(this.ble)
-		},
-		'tap .btn-negative':function(e){
-			if (!this.peripheral) return
-			var data = new Uint8Array(1);
-			data[0] = 1;
-			this.signals.ble_write(this.peripheral.id, scratchSrv, scratch1, data.buffer, function(){
-				debugger
-				console.log(arguments)
+				self.signals.ble_write(p.id, scratchSrv, scratch2, cred.buffer.slice(4, 8), function(){
+					console.log(arguments)
+				}).send(self.ble)
 			}).send(this.ble)
 		}
 	}
