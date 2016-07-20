8cef5af7fadf4813986e4fef98a4dabd82e5fcdf
Darren Liew <ldarren@gmail.com>
2016-07-20 11:08:36 +0000

    fixed add button disappear

--- apps/lead/app/mod/ListViewLockerMixin.js
+++ apps/lead/app/mod/ListViewLockerMixin.js
@@ -1,14 +1,16 @@
 return {
 	signals:['headerButtonClicked'],
 	events:{
-		'click button.addLocker':function(e){
+		'click .addLocker':function(e){
 			this.signals.headerButtonClicked('plus').send(this.host)
 		}
 	},
 	postRender:function(){
-		var btn=document.createElement('button')
-		btn.classList.add('addLocker')
-		btn.textContent='Add Locker'
-		this.el.insertBefore(btn, this.el.firstChild);
+		var div=document.createElement('div')
+		div.classList.add('addLocker')
+		div.classList.add('button')
+		div.classList.add('card')
+		div.textContent='Add Locker'
+		this.el.insertBefore(div, this.el.firstChild);
 	}
 }
--- apps/lead/app/mod/ListViewRequestMixin.js
+++ apps/lead/app/mod/ListViewRequestMixin.js
@@ -1,14 +1,16 @@
 return {
 	signals:['headerButtonClicked'],
 	events:{
-		'click button.addRequest':function(e){
+		'click .addRequest':function(e){
 			this.signals.headerButtonClicked('plus').send(this.host)
 		}
 	},
 	postRender:function(){
-		var btn=document.createElement('button')
-		btn.classList.add('addRequest')
-		btn.textContent='Add Request'
-		this.el.insertBefore(btn, this.el.firstChild);
+		var div=document.createElement('div')
+		div.classList.add('button')
+		div.classList.add('card')
+		div.classList.add('addRequest')
+		div.textContent='Add Request'
+		this.el.insertBefore(div, this.el.firstChild);
 	}
 }
