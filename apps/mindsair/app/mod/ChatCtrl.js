f668b5bfd2b3863d354a0cf9bd586df82c726a2a
Darren Liew <ldarren@gmail.com>
2016-08-01 11:14:26 +0000

    added mobile ver

--- apps/mindsair/app/mod/ChatCtrl.js
+++ apps/mindsair/app/mod/ChatCtrl.js
@@ -1,10 +1,12 @@
 return {
 	signals:['hideChat'],
 	deps:{
+		showByDefault:['bool',false]
 	},
 	create:function(deps){
 		var layer0=document.querySelector('#mindsair #layer0')
 		this.layer0=layer0.classList
+		if (deps.showByDefault) this.layer0.remove('hidden')
 	},
 	slots:{
 		showChat:function(from,sender){
