bcdb8d7f6a1e4cc03745d923d7ddfac3cb6f39c9
Darren Liew <ldarren@gmail.com>
2016-09-19 11:28:42 +0000

    working signup and signin ui

--- apps/jasy/app/mod/DialogAuth.js
+++ apps/jasy/app/mod/DialogAuth.js
@@ -1,22 +1,71 @@
+var
+picoStr=require('pico/str'),
+signin=function(from,sender,data){
+	this.deps.cred.read(null,{
+		data:{
+			email:data.email,
+			pwd:picoStr.hash(data.pwd)
+		},
+		success:function(){
+			console.log('signup suceed')
+		},
+		error:function(){
+			console.log('signup failed')
+		}
+	})
+},
+signup=function(from,sender,data){
+	if (data.pwd!==data.confirm_pwd) return __.dialogs.alert('Password not matched','Sign Up Error')
+	this.deps.cred.create(null,{
+		data:{
+			name:data.name,
+			email:data.email,
+			pwd:picoStr.hash(data.pwd)
+		},
+		success:function(){
+			console.log('signup suceed')
+		},
+		error:function(){
+			console.log('signup failed')
+		}
+	})
+},
+dummyPageResult=function(from,sender,data,cb){ cb() }
+
 return {
+	signals:['pageChange','collectPageResult'],
 	deps:{
-		formSignin:'list',
-		formSignout:'list'
+		form:'list',
+		cred:'models'
 	},
 	slots:{
 		signin:function(from,sender,user){
 			this.slots.modalHide.call(this,from,this)
 		},
 		signout:function(from,sender){
-			this.slots.modalShow.call(this,from,this,this.deps.formSignin)
+			this.slots.modalShow.call(this,from,this,this.deps.form)
 		},
 		pageCreate:function(from,sender,curr,total,page,cb){
 			cb(null,page)
 		},
-		pageChange:function(from,sender,name,value,cb){
+		pageItemChange:function(from,sender,name,value,cb){
+			var self=this
+			switch(name){
+			case 'signin':
+				this.slots.pageResult=signin
+				return this.signals.collectPageResult().send(sender)
+			case 'signup':
+				this.slots.pageResult=signup
+				return this.signals.collectPageResult().send(sender)
+			case 'gosignin':
+				this.slots.pageResult=dummyPageResult
+				return this.signals.pageChange(0,false).send(sender)
+			case 'gosignup':
+				this.slots.pageResult=dummyPageResult
+				return this.signals.pageChange(1,false).send(sender)
+			}
 		},
-		pageResult:function(from,sender,data,cb){
-		},
+		pageResult:dummyPageResult,
 		modalResult:function(from,sender,data){
 		}
 	}
--- apps/jasy/node/api/user.js
+++ apps/jasy/node/api/user.js
@@ -1,13 +1,18 @@
 const
 SUBJECT='Confirmation',
-tmplTxt=require('tmpl/email_confirmation.txt'),
-tmplHtml=require('tmpl/email_confirmation.html')
+TEXT_CONFIRM=require('tmpl/email_confirmation.txt'),
+HTML_CONFIRM=require('tmpl/email_confirmation.html'),
+URL_CONFIRM_DEV='https://st.jasaws.com/jasy/app/#/email/confirm/VID',
+URL_CONFIRM_PRO='https://console.jasaws.com/jasy/app/#email/confirm/VID',
+URL_DOC_DEV='https://st.jasaws.com/jasy/app/#doc',
+URL_DOC_PRO='https://console.jasaws.com/jasy/app/#doc'
 
 var
 picoStr=require('pico/str'),
 sqlUser=require('sql/user'),
 redisUser=require('redis/user'),
 ses,
+isPro=true,
 createKey=function(){
 	return picoStr.rand().substr(0,20)
 },
@@ -17,6 +22,7 @@ createSecret=function(){
 
 return {
 	setup(context,cb){
+		isPro='pro'===pico.env('env')
 		ses=context.email.ses
 		cb()
 	},
@@ -36,30 +42,39 @@ return {
 	signup(input,output,next){
 		var email=input.email
 		if (!email || !input.pwd) return next(this.error(400))
-		var verifyId=createKey()
-		ses.send(
-		[email],
-		SUBJECT,
-		tmplTxt.replace('VERIFYID',verifyId),
-		{html:tmplHtml.replace('VERIFYID',verifyId)},
-		(err,data)=>{
-			if (err) return next(this.error(400, err))
-			Object.assign(output,input,{pwd:0})
-			redisUser.setRegisterCache(email,verifyId,input)
-			next()
-		})
+		var
+		verifyId=createKey(),
+		urlConfirm=(isPro?URL_CONFIRM_PRO:URL_CONFIRM_DEV).replace('VID',verifyId),
+		urlDoc=isPro?URL_DOC_PRO:URL_DOC_DEV
+
+		ses.send([email],SUBJECT,
+			TEXT_CONFIRM.replace('URL_EMAIL_CONFIRM',urlConfirm).replace('URL_DOC',urlDoc),
+			{html:HTML_CONFIRM.replace('URL_EMAIL_CONFIRM',urlConfirm).replace('URL_DOC',urlDoc)},
+			(err,data)=>
+			{
+				if (err) return next(this.error(400, err))
+				Object.assign(output,input,{pwd:0})
+				redisUser.setRegisterCache(email,verifyId,input)
+				next()
+			})
 	},
 	signin(input,next){
+		next()
 	},
 	signout(input,next){
+		next()
 	},
 	read(input,next){
+		next()
 	},
 	update(input,next){
+		next()
 	},
 	remove(input,next){
+		next()
 	},
 	verify(input,next){
+		next()
 	},
 	confirmEmail(input,output,next){
 		var
