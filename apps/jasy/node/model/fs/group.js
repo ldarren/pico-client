c55c2d03c861c8a6b253ab32378a3d838ab34016
Darren Liew <ldarren@gmail.com>
2016-10-19 18:00:09 +0000

    added checkright

--- apps/jasy/node/model/fs/group.js
+++ apps/jasy/node/model/fs/group.js
@@ -9,60 +9,83 @@ DIR_API=		'.api/',
 DIR_APP=		'.app/',
 DIR_EPI=		'.epi/',
 DIR_EPP=		'.epp/',
-DIR_GRP=		'.grp/'
+DIR_GRP=		'.grp/',
 
-let
-fsdb,TYPE,MODE,META,
-newGroup=function(root, cby, cb){
-	fsdb.createp(root, 'genesis', TYPE.DIR, MODE.G_X | MODE.A_X, (err)=>{
+addRight=function(db,group,id,right,meta,cb){
+	db.create(group, right, meta, TYPE.DIR, MODE.G_X | MODE.A_X, (err)=>{
 		if (err) return cb(err)
-		fsdb.create(root, DIR_ROOT, 'root', TYPE.DIR, MODE.G_X | MODE.A_X, (err)=>{
+		if (!id) return cb()
+		id=id.toString()
+		db.createp(db.join(group,right,id),db.path(id),TYPE.LINK, MODE.G_X | MODE.A_X, cb)
+	})
+},
+checkRight=function(db,id,right,root,url,cb){
+	let
+	us=db.split(url),
+	urls=[]
+	while(us.length){
+		urls.push(db.join(...us,right))
+		urls.pop()
+	}
+	db.findLink(db.path(id),root,urls,cb)
+},
+newGroup=function(db, root, meta, cby, cb){
+	db.createp(root, meta, TYPE.DIR, MODE.G_X | MODE.A_X, (err)=>{
+		if (err) return cb(err)
+		addRight(db, root, cby, DIR_ROOT, 'root', (err)=>{
 			if (err) return cb(err)
-			cby=cby.toString()
-			fsdb.create(fsdb.join(root,DIR_ROOT),cby,fsdb.path(cby),TYPE.LINK, MODE.G_X | MODE.A_X, (err)=>{
+			addRight(db, root, cby, DIR_SUDO, 'sudo', (err)=>{
 				if (err) return cb(err)
-				fsdb.create(root, DIR_NOOB, 'noob', TYPE.DIR, MODE.G_X | MODE.A_X, cb)
+				addRight(db, root, cby, DIR_EDIT, 'edit', (err)=>{
+					if (err) return cb(err)
+					addRight(db, root, cby, DIR_READ, 'read', (err)=>{
+						if (err) return cb(err)
+						addRight(db, root, 0, DIR_NOOB, 'noob', cb)
+					})
+				})
 			})
 		})
 	})
 }
 
+let db,TYPE,MODE,META
+
 module.exports= {
     setup(context, cb){
-        fsdb=context.group
-		TYPE=fsdb.TYPE
-		MODE=fsdb.MODE
-		META=fsdb.META
-
-		// TODO: move this to separate tool?
-		fsdb.createp('0', 'adam', TYPE.DIR, MODE.G_X | MODE.A_X, (err)=>{
-			if (err) cb(err)
-			newGroup('0/jasy', 0, cb)
-		})
+        db=context.group
+		TYPE=db.TYPE
+		MODE=db.MODE
+		META=db.META
 	},
 	createSession(session,cb){
-		fsdb.create(fsdb.path(session.id), session.sess, TYPE.FILE, MODE.G_X | MODE.A_X, cb)
+		db.create(db.path(session.id), session.sess, TYPE.FILE, MODE.G_X | MODE.A_X, cb)
 	},
 	joinGroup(session,cb){
 		let
 		uid=session.id.toString(),
-		u=fsdb.path(uid),
-		grp=fsdb.join(u,session.grp)
+		u=db.path(uid),
+		grp=db.join(u,session.grp)
 
-		fsdb.mode(grp, (err,mode)=>{
+		db.mode(grp, (err,mode)=>{
 			if (!err) return cb(err)
 			if (!mode) return cb(`${grp} not found`)
-			let noob=fsdb.join(grp,DIR_NOOB)
-			fsdb.mode(noob, (err,mode)=>{
-				if (mode) return fsdb.create(fsdb.join(noob,uid),u,TYPE.LINK, MODE.G_X | MODE.A_X, cb)
-				fsdb.create(fsdb.join(grp,uid),u,TYPE.LINK, MODE.G_X | MODE.A_X, cb)
+			let noob=db.join(grp,DIR_NOOB)
+			db.mode(noob, (err,mode)=>{
+				if (mode) return db.create(db.join(noob,uid),u,TYPE.LINK, MODE.G_X | MODE.A_X, cb)
+				db.create(db.join(grp,uid),u,TYPE.LINK, MODE.G_X | MODE.A_X, cb)
 			})
 		})
 	},
-	createGroup(name,meta,cby,cb){
-		fsdb.create(fsdb.path(name), meta, TYPE.DIR, MODE.G_X | MODE.A_X, cb)
+	createGroup(root,name,meta,cby,cb){
+		checkRight(db,cby,DIR_ROOT,root,name,(err)=>{
+			if (err) return cb(err)
+			newGroup(root+name,meta,cby,cb)
+		})
 	},
-	removeGroup(name,cb){
-		fsdb.remove(fsdb.path(name)+META,cb)
+	removeGroup(root,name,cby,cb){
+		checkRight(db,cby,DIR_ROOT,root,name,(err)=>{
+			if (err) return cb(err)
+			db.remove(db.path(root,name),cb)
+		})
 	}
 }
