155a1d7915125329db42cbb292fd14ecac6c44b4
Darren Liew <ldarren@gmail.com>
2016-12-09 18:53:48 +0000

    added user profile

--- apps/glueon/app/mod/ProfileCtrl.js
+++ apps/glueon/app/mod/ProfileCtrl.js
@@ -6,12 +6,26 @@ return{
 		btnRight:'map',
 		cred:'models',
 		users:'models',
-		owner:'models'
+		owner:'models',
+		userProfile:'list'
 	},
 	create: function(deps){
+		var
+		me=deps.cred.at(0),
+		up=deps.userProfile,
+		name
+		if (me){
+			var user=deps.users.get(me.id)
+			name=user.get('name')
+			deps.owner.set(user)
+			up.push({model:'owner',field:1,value:'name',label:'Name'})
+			up.push({type:'button',name:'signup',label:'Sign up'})
+		}else{
+			up.push({type:'button',name:'signin',label:'Sign in'})
+		}
 		if(deps.title)this.signals.header(
 			deps.paneId,
-			deps.title,
+			name||deps.title,
 			0===deps.paneId?{icon:'icon_back'}:deps.btnLeft,
 			deps.btnRight
 		).send(this.host)
