a84a58d0d80e57962d488ff8b08d982d53f07ba4
Darren Liew <ldarren@gmail.com>
2017-08-22 17:08:06 +0000

    fixed view or ctrl deps, fixed missing model in specMgr

--- example/mod/Frame.js
+++ example/mod/Frame.js
@@ -1,10 +1,15 @@
 var router = require('po/router')
 var specMgr = require('p/specMgr')
+var specMap = null
 
 function pageChanged(evt, state, params){
 	var p = this.modules.pop()
 	p && p.remove()
-	this.spawnBySpec([state], params)
+	var spec = []
+	for (var i=0, k; k=state[i]; i++){
+		spec.push(specMap[k])
+	}
+	this.spawnBySpec(spec, params)
 }
 
 return {
@@ -15,18 +20,17 @@ return {
 	create: function(deps, params){
 		this.super.create.call(this, deps, params)
 
+		specMap = {}
 		var rawSpec = []
 		var keys = Object.keys(deps.pages)
 		for (var i=0,k; k=keys[i]; i++){
 			rawSpec.push(deps.pages[k])
 		}
 		specMgr.load(this, params, rawSpec, (err, spec)=>{
-			var routes={}
-			for (var i=0,k; k=keys[i]; i++){
-				routes[k] = spec[i]
+			for (var i=0, k; k=keys[i]; i++){
+				specMap[k]=spec.pop()
 			}
-
-			router.on('change',pageChanged,this).start(routes)
+			router.on('change',pageChanged,this).start(deps.routes)
 		})
 	}
 }
