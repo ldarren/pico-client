32d29606881d307f2f58a6a017643b16b5e2b44f
Darren Liew <ldarren@gmail.com>
2017-08-27 13:44:27 +0000

    simplified sigslot

--- example/mod/Frame.js
+++ example/mod/Frame.js
@@ -1,6 +1,5 @@
 var router = require('po/router')
 var specMgr = require('p/specMgr')
-var specMap = null
 
 function remove(modules){
 	if (!modules.length) return
@@ -9,13 +8,17 @@ function remove(modules){
 	remove(modules)
 }
 
-function pageChanged(evt, state, params){
-	remove(this.modules)
+function actual(self, state, params){
+	remove(self.modules)
 	var spec = []
 	for (var i=0, k; k=state[i]; i++){
-		spec.push(specMap[k])
+		spec.push(self.specMap[k])
 	}
-	this.spawnBySpec(spec, params)
+	self.spawnBySpec(spec, params)
+}
+
+function pageChanged(evt, state, params){
+	setTimeout(actual, 10, this, state, params)
 }
 
 return {
@@ -26,12 +29,13 @@ return {
 	create: function(deps, params){
 		this.super.create.call(this, deps, params)
 
-		specMap = {}
 		var rawSpec = []
 		var keys = Object.keys(deps.pages)
 		for (var i=0,k; k=keys[i]; i++){
 			rawSpec.push(deps.pages[k])
 		}
+
+		var specMap = this.specMap = {}
 		specMgr.load(this, params, rawSpec, (err, spec)=>{
 			for (var i=0, k; k=keys[i]; i++){
 				specMap[k]=spec.shift()
