7612a46ddca0c422025ef40a81d57862661fbb6d
Darren Liew <ldarren@gmail.com>
2018-06-03 17:25:08 +0000

    updated spa exmaple to latest pico-client

--- examples/bundle/main/bundle.js
+++ examples/bundle/main/bundle.js
@@ -1,33 +1,33 @@
 pico.run({
-  name: 'bundle',
-  ajax: __.ajax,
-  onLoad: __.load,
-  env:{
-    live:false,
-    dataset:(function(el){ if (el) return el.dataset })(document.getElementById('picoEnv'))
-  },
-  preprocessors:{
-    '.asp':function(url,asp){ return pico.export('pico/str').template(asp) }
-  },
-  paths:{
-    '~': './mod/',
-    root: './',
-	main: './main/',
-    cfg: './cfg/',
-    p: './lib/pico/',
-    po: './lib/pojs/'
-  }
+	name: 'bundle',
+	ajax: __.ajax,
+	onLoad: __.load,
+	env:{
+		live:false,
+		dataset:(function(el){ if (el) return el.dataset })(document.getElementById('picoEnv'))
+	},
+	preprocessors:{
+		'.asp':function(url,asp){ return pico.export('pico/str').template(asp) }
+	},
+	paths:{
+		'~': './mod/',
+		root: './',
+		main: './main/',
+		cfg: './cfg/',
+		p: './lib/pico/',
+		po: './lib/pojs/'
+	}
 },function(){
-  var specMgr= require('p/specMgr')
-  var View= require('p/View')
-  var project = require('cfg/proj-bundle.json')
-  var main
+	var specMgr= require('p/specMgr')
+	var View= require('p/View')
+	var project = require('cfg/proj-bundle.json')
+	var main
 
-  return function(){
-    specMgr.load(null, null, project, function(err, spec){
-      if (err) return console.error(err)
-      main = new View
-      main.spawnBySpec(spec)
-    })
-  }
+	return function(){
+		specmgr.load(null, null, project, function(err, spec){
+			if (err) return console.error(err)
+			main = new view
+			main.spawnbyspec(spec)
+		})
+	}
 })
